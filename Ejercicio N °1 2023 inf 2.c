5307ac10756fe41ea39cfa12d73aec6e23d9bf92
MatiCostaH <128419602+MatiCostaH@users.noreply.github.com>
2023-03-23 18:17:17 -0300

    Add files via upload

---
+++ Ejercicio N °1 2023 inf 2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+	int n = 100;
+	int notas[n];
+	int valor;
+	int contador = 0;
+	int suma = 0;
+	
+	for(int i=0;i<=n;i++){
+		printf("ingrese una nota:");
+		scanf("%d",&valor);
+		{
+		if(valor==9999)
+		{break;
+		}
+		}
+		notas[i]=valor;
+		contador ++;
+	}
+	for(int i=0;i<contador;i++){
+		suma+= notas[i];
+		printf("\n el total de la sumatoria es:%d\n",suma);
+	}
+	float promedio= suma/contador;
+	printf("\n el promedio de los números ingresados es:%f",promedio);
+	return 0;
+}
+
