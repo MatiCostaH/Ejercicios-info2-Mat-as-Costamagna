6ec7a49eb8b9b78fe7ded07f5907e426b2c3275d
MatiCostaH <128419602+MatiCostaH@users.noreply.github.com>
2023-07-05 11:52:00 -0300

    Update EjercicioPCN10.c

--- EjercicioPCN10.c
+++ EjercicioPCN10.c
@@ -4,31 +4,33 @@
 #include "rs232.h"
 
 #define MAX_BUFFER_SIZE 256
+#define PUERTO_COM 3
+#define BAUDIOS 9600
+
 
 int main() {
-  int CantidadByte;
-  int puertoCOM= 3;             //Número de puerto. 4 es para el COM3 en windows
-  int baudios=9600;            //Velocidad en baudios
-  char modo[]={'8','N','1',0}; // 8 bits de datos, no paridad, 1 bit de parada
-  char Recepcion[LONG_BUFFER];
-  char str[2][512];
-
-   strcpy(str[0], "A");
-  strcpy(str[1], "E");
-
-  if(RS232_OpenComport(puertoCOM, baudios, modo, 0)) //Prueba abrir el puerto, devuelve 1 en caso de error
-  {
-    printf("No se puede abrir el puerto COM\n");
-    return(0);
-  }
-
+	int bytesWritten, bytesRead;
+	int port_num = PUERTO_COM;
+	int baudrate = BAUDIOS;
+	char mode[] = {'8','N','1',0};
+	char buffer[MAX_BUFFER_SIZE];
+	char comando[2][512];
+	
+	strcpy(comando[0], "A");
+	strcpy(comando[1], "E");
+	
+	if (RS232_OpenComport(port_num, baudrate, mode, 0)) {
+		printf("No se puede abrir el puerto COM\n");
+		return 0;
+	}
+	
 	for (int i = 0; i < 2; i++) {
 		// Enviar comandos al Arduino
 		printf("Ingrese un comando ('E' para encender, 'A' para apagar): ");
-		scanf("%s", comando);
+		scanf("%s", comando[i]);
 		
 		// Enviar el comando al Arduino
-		bytesWritten = RS232_SendBuf(port_num, comando, strlen(comando));
+		bytesWritten = RS232_SendBuf(port_num, comando[i], strlen(comando[i]));
 		if (bytesWritten < 0) {
 			printf("Error al enviar el comando al Arduino.\n");
 			RS232_CloseComport(port_num);
